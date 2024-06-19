df58589a6eb0f7a674cccf63e02e92b683656d12
shiraadler <shira.adler@mail.huji.ac.il>
2024-06-19 18:08:31 +0300

    main

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from movingVSstat import cancel_moving_cars
 from yolo import predict
 from parkingAreaIdentification import parking_mark
 from linearSeparator import find_linear_separator
+from segmentation import *
+from SAMworld import *  # only for testing and displaying
 
 PARKING_AREA = {1: [], 2: [], 3: []}
 SUPERVISED = False
-
 
 # Step 1: Preprocessing video of destination
 path1, path2 = preprocessing("Scenes\scene1")
@@ -18,15 +19,28 @@ detections1, annotated_image1 = predict(path1)
 detections2, annotated_image2 = predict(path2)
 
 # Step 3: Cancelling moving cars
-detections, annotated_image = cancel_moving_cars(detections1, annotated_image1, detections2, annotated_image2)
+detections, annotated_image = cancel_moving_cars(detections1, annotated_image1,
+                                                 detections2, annotated_image2)
 
 # Step 4: Distinguishing the parking areas
 if SUPERVISED:
     # TODO: Shira
-    annotated_image = parking_mark(detections, annotated_image, PARKING_AREA[1])
+    annotated_image = parking_mark(detections, annotated_image,
+                                   PARKING_AREA[1])
 
 else:
     annotated_image = find_linear_separator(detections, annotated_image)
 
-# TODO: Shira
-# Step 5: Detecting empty parking spots
+    # TODO: Shira
+    # Step 5: Detecting empty parking spots
+
+    smallest_car_in_scene = find_smallest_car(detections)
+
+    exact_detections = get_edge_points(detections, annotated_image)
+
+    # displaying the points
+    for cord in exact_detections:
+        display_edge_points(annotated_image, exact_detections)
+
+    free_parking_between_cars(exact_detections, smallest_car_in_scene)
+
