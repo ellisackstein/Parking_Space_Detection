fffbcba8c504bd9a731624e70f7a6a53ac2ee69d
Elli Sackstein <elli.sackstein@mail.huji.ac.il>
2024-06-28 18:51:43 +0300

    .

--- yolo.py
+++ yolo.py
@@ -66,11 +66,13 @@ def predict(path):
     results = model.infer(image)
     detections = sv.Detections.from_inference(results)
 
-    BOUNDING_BOX_ANNOTATOR = sv.BoundingBoxAnnotator(thickness=2)
+    BOUNDING_BOX_ANNOTATOR = sv.BoundingBoxAnnotator(thickness=5)
     LABEL_ANNOTATOR = sv.LabelAnnotator(text_thickness=2, text_scale=1, text_color=sv.Color.BLACK)
     annotated_image = image.copy()
     annotated_image = BOUNDING_BOX_ANNOTATOR.annotate(annotated_image, detections)
     annotated_image = LABEL_ANNOTATOR.annotate(annotated_image, detections)
-    # sv.plot_image(annotated_image, (10, 10))
+    sv.plot_image(annotated_image, (10, 10))
 
     return detections, annotated_image
+
+predict("test_img/sss.jpg")
