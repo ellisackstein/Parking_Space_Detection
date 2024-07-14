dbf1dc6e454854d1003c376008c423e9a30a779b
Elli Sackstein <elli.sackstein@mail.huji.ac.il>
2024-07-14 16:37:22 +0300

    added if __name__ == '__main__':

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # test_path = "Tests/empty_spots/scene2/test12/1.png"
+import sys
 from preprocessing import preprocessing
 from movingVSstat import cancel_moving_cars
 from yolo import *
@@ -6,30 +7,44 @@ from parkingAreaIdentification import *
 from linearSeparator import find_linear_separator
 from SAMworld import *  # only for testing and displaying
 from emptySpots import *
+CONFIGURED = "configured"
 
-CONFIGURED = True
+if __name__ == '__main__':
+    # Our algorithm gets: scene ID (for collecting parking zones),
+    # Method (configured, non-configured) and path.
+    path = sys.argv[1]
+    method = sys.argv[2]
+    scene_id = sys.argv[3]
 
-# Step 1: Preprocessing video of destination
-path1, path2 = preprocessing("Scenes\scene1")
+    # The configured method
+    if method == CONFIGURED:
 
-# step 2: Get predictions
-# detections1, masks1, annotated_image1 = predict(path1)
-# detections2, masks2, annotated_image2 = predict(path2)
+        # Step 1 : Get predictions
+        detections, masks, annotated_image = predict(path)
 
-detections, masks, annotated_image = predict(path2)
+        # Step 2 : Distinguishing the parking areas
+        parking_areas = parking_mark(scene_id, "Parking_areas")
 
-# Step 3: Cancelling moving cars
-# detections, masks, annotated_image = cancel_moving_cars(detections1, masks1, annotated_image1,
-#                                                         detections2, masks2, annotated_image2)
+        # Step 3 : Distinguishing the parking areas
+        free_spots, free_areas = find_empty_spots(annotated_image, detections, masks, parking_areas)
+        present_results(free_spots, path)
 
-# Step 4: Distinguishing the parking areas
-if CONFIGURED:
-    scene_num = 1
-    parking_areas = parking_mark(scene_num, "Parking_areas")
+    # The non-configured method
+    else:
+        # Step 1: Preprocessing video of destination
+        path1, path2 = preprocessing(path)
 
-else:  # if not supervised, find a linear separator
-    parking_areas = find_linear_separator(detections, annotated_image)
+        # step 2: Get predictions
+        detections1, masks1, annotated_image1 = predict(path1)
+        detections2, masks2, annotated_image2 = predict(path2)
 
-# Step 5: Detecting empty parking spots
-free_spots, free_areas = find_empty_spots(annotated_image, detections, masks, parking_areas)
-present_results(free_spots, path1)
+        # Step 3: Cancelling moving cars
+        detections, masks, annotated_image = cancel_moving_cars(detections1, masks1, annotated_image1,
+                                                                detections2, masks2, annotated_image2)
+
+        # Step 4 : if not configured, find a linear separator that defines the parking zones
+        parking_areas = find_linear_separator(detections, annotated_image)
+
+        # Step 5: Detecting empty parking spots
+        free_spots, free_areas = find_empty_spots(annotated_image, detections, masks, parking_areas)
+        present_results(free_spots, path1)
